d1f4cff9982ff262f1646d92288541e39234ba50
Banadesh <bannusbali@gmail.com>
2023-06-24 20:23:34 +0530

    Added Entity

--- src/main/java/com/example/Springapp/domain/Author.java
+++ src/main/java/com/example/Springapp/domain/Author.java
@@ -1,6 +1,15 @@
 package com.example.Springapp.domain;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.util.Set;
+@Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String Title;
     private int isbn;
     private Set<Books> book ;
@@ -12,6 +21,14 @@ public class Author {
         Title = title;
         this.isbn = isbn;
         this.book = book;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
--- src/main/java/com/example/Springapp/domain/Books.java
+++ src/main/java/com/example/Springapp/domain/Books.java
@@ -1,6 +1,15 @@
 package com.example.Springapp.domain;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.util.Set;
+@Entity
 public class Books {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String FirstName ;
     private String LastName;
     private Set<Author> authors;
@@ -12,6 +21,14 @@ public class Books {
         FirstName = firstName;
         LastName = lastName;
         this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
